e8c4c713fdd2421cc57df51da50f5bbe2d8a10a0
yuz97 <51846940+yuz97@users.noreply.github.com>
2020-01-30 18:34:34 +0800

    Add files via upload

---
+++ belajar_membuat_qrcode.py
@@ -0,0 +1,24 @@
+""" untuk bisa menggunakan atau memanggil fungsi ini pastikan 
+laptop mu sudah terinstall Python3 
+langkah-langkahnya :
+1.pip install qrcode :
+	menginstall qrcode terlebih dahulu
+"""
+
+#cara eksekusi
+
+import qrcode
+
+a = qrcode.make(f'https//:www.instagram.com') #https//... bisa diganti sesuai keinginan
+a.save('instagram.jpg') #bisa diganti sesuai keinginan contoh 'alif.jpg/jpeg atau alif.png'
+
+
+
+'''
+created by : Yusri Warrayhan
+Jurusan : Teknik Elektro
+Fakultas Teknik Universitas Teknologi Sulawesi Makassar
+Jl.Talasapang NO.51 Makassar
+
+selamat belajar dan tetap semangat
+'''
